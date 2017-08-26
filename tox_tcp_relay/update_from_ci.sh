6045e3c2e9b37e3a25c8e6709725d04eff92a1b5
Zoff <zoff99@users.noreply.github.com>
2017-08-26 20:48:43 +0200

    script fixes

--- tox_tcp_relay/update_from_ci.sh
+++ tox_tcp_relay/update_from_ci.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 #####################################################
-# update toxcam binary from Circle CI (master branch)
+# update binary from Circle CI (master branch)
 #####################################################
 
 cd $(dirname "$0")
 
-pkill toxcam_static # will stop toxcam
-cp -av toxcam_static toxcam_static__BACKUP
-wget -O toxcam_static 'https://circleci.com/api/v1/project/zoff99/ToxCam/latest/artifacts/0/$CIRCLE_ARTIFACTS/RASPI/toxcam_static?filter=successful&branch=master'
-chmod u+rwx toxcam_static
+pkill tox_tcp_relay_static # will stop tox tcp-relay
+cp -av tox_tcp_relay_static tox_tcp_relay_static__BACKUP
+wget -O tox_tcp_relay_static 'https://circleci.com/api/v1/project/zoff99/ToxTCP-RelayNode/latest/artifacts/0/$CIRCLE_ARTIFACTS/ubuntu_14_04_binaries/tox_tcp_relay_static?filter=successful&branch=master'
+chmod u+rwx tox_tcp_relay_static
 
