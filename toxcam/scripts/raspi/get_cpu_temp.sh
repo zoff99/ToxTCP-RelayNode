1f271c0d89c72b2ecd345853d34e37a02e81534e
Zoff <zoff99@users.noreply.github.com>
2017-08-26 16:13:10 +0200

    initial setup

--- toxcam/scripts/raspi/get_cpu_temp.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cpu1=$(</sys/class/thermal/thermal_zone0/temp)
-cpu_int=$((cpu1/1000))
-
-echo $cpu_int
--- toxcam/scripts/raspi/get_gpu_temp.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-gpu1=$(/opt/vc/bin/vcgencmd measure_temp)
-gpu=$(echo "$gpu1"|sed -e 's#^.*=##'|sed -e 's#.C.*$##')
-# gpu_int=$(echo "$gpu1"|sed -e 's#^.*=##'|sed -e 's#\..*$##')
-
-echo $gpu
