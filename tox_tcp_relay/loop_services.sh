7f57b3a9892cb9fc082ac2271f720b8347a3f8bf
Zoff <zoff99@users.noreply.github.com>
2017-08-30 14:11:55 +0200

    Update loop_services.sh

--- tox_tcp_relay/loop_services.sh
+++ tox_tcp_relay/loop_services.sh
@@ -3,8 +3,6 @@
 cd $(dirname "$0")
 
 while [ 1 == 1 ]; do
-	# ./tox_tcp_relay_static -2 # > /dev/null 2> /dev/null
-    ./tox-bootstrapd_static --foreground --log-backend=stdout --config=tox-bootstrapd.conf
-	sleep 10
+    ./tox-bootstrapd_static --foreground --log-backend=stdout --config=tox-bootstrapd.conf >> ./tox-bootstrapd.log 2>&1
+    sleep 10
 done
-
