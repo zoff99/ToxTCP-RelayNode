342adc3f94bdcecd8cee59dd2b7625d61a38d601
Zoff <zoff99@users.noreply.github.com>
2017-08-30 14:10:57 +0200

    Update update_from_ci.sh

--- tox_tcp_relay/update_from_ci.sh
+++ tox_tcp_relay/update_from_ci.sh
@@ -6,8 +6,8 @@
 
 cd $(dirname "$0")
 
-pkill tox_tcp_relay_static # will stop tox tcp-relay
-cp -av tox_tcp_relay_static tox_tcp_relay_static__BACKUP
-wget -O tox_tcp_relay_static 'https://circleci.com/api/v1/project/zoff99/ToxTCP-RelayNode/latest/artifacts/0/$CIRCLE_ARTIFACTS/ubuntu_14_04_binaries/tox_tcp_relay_static?filter=successful&branch=master'
-chmod u+rwx tox_tcp_relay_static
+pkill tox-bootstrapd_static # will stop bootstrapd
+cp -av tox-bootstrapd_static tox-bootstrapd_static__BACKUP
+wget -O tox-bootstrapd_static 'https://circleci.com/api/v1/project/zoff99/ToxTCP-RelayNode/latest/artifacts/0/$CIRCLE_ARTIFACTS/ubuntu_14_04_binaries/tox-bootstrapd_static?filter=successful&branch=master'
+chmod u+rwx tox-bootstrapd_static
 
